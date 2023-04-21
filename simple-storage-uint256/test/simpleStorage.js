2a2f3eac0751499eed0268521b26e786350589d9
damartripamungkas <damartripamungkas@github.com>
2023-04-22 01:41:52 +0700

    feat: change code test

--- simple-storage-uint256/test/simpleStorage.js
+++ simple-storage-uint256/test/simpleStorage.js
@@ -1,17 +1,26 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
 
 contract("SimpleStorage", function (accounts) {
-    it("try method increase and decrease", async () => {
-        let getQtyMyBook;
-        const contractInstance = await SimpleStorage.new(); // will create smart contract and send to blockchain
+    const initContract = async () => {
+        return await SimpleStorage.new();
+    };
+
+    it("try increase my book", async () => {
+        const contractInstance = await initContract(); // will create smart contract and send to blockchain
 
         await contractInstance.increaseMyBook(2); // I am trying to increase the number of books by 2
-        getQtyMyBook = await contractInstance.qtyMyBook.call(accounts[0]);
-        console.log("🚀 ~ file: SimpleStorage.js:10 ~ it ~ contractInstance:", getQtyMyBook.toString()); // result getQtyMyBook = 2
+        const getQtyMyBook = await contractInstance.qtyMyBook.call(accounts[0]);
+        console.log("🚀 ~ file: SimpleStorage.js:13:", getQtyMyBook.toString()); // result getQtyMyBook = 2
+
+        return assert.isTrue(true);
+    });
+
+    it("try decrease my book", async () => {
+        const contractInstance = await initContract(); // will create smart contract and send to blockchain
 
         await contractInstance.decreaseMyBook(1); // I want to reduce the number of books by 1
-        getQtyMyBook = await contractInstance.qtyMyBook.call(accounts[0]);
-        console.log("🚀 ~ file: SimpleStorage.js:14 ~ it ~ contractInstance:", getQtyMyBook.toString()); // result getQtyMyBook = 1
+        const getQtyMyBook = await contractInstance.qtyMyBook.call(accounts[0]);
+        console.log("🚀 ~ file: SimpleStorage.js:23:", getQtyMyBook.toString()); // result getQtyMyBook = 1
 
         return assert.isTrue(true);
     });
