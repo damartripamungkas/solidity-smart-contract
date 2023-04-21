6dec2744f445156042de25bf01b500eca77b3d59
damartripamungkas <damartripamungkas@github.com>
2023-04-22 01:47:48 +0700

    fix: error in test

--- simple-storage-uint256/test/simpleStorage.js
+++ simple-storage-uint256/test/simpleStorage.js
@@ -1,13 +1,13 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
 
 contract("SimpleStorage", function (accounts) {
-    const initContract = async () => {
-        return await SimpleStorage.new();
-    };
+    let contractInstance;
+    it("try create smart contract", async () => {
+        contractInstance = await SimpleStorage.new();
+        return assert.isTrue(true);
+    });
 
     it("try increase my book", async () => {
-        const contractInstance = await initContract(); // will create smart contract and send to blockchain
-
         await contractInstance.increaseMyBook(2); // I am trying to increase the number of books by 2
         const getQtyMyBook = await contractInstance.qtyMyBook.call(accounts[0]);
         console.log("🚀 ~ file: SimpleStorage.js:13:", getQtyMyBook.toString()); // result getQtyMyBook = 2
@@ -16,8 +16,6 @@ contract("SimpleStorage", function (accounts) {
     });
 
     it("try decrease my book", async () => {
-        const contractInstance = await initContract(); // will create smart contract and send to blockchain
-
         await contractInstance.decreaseMyBook(1); // I want to reduce the number of books by 1
         const getQtyMyBook = await contractInstance.qtyMyBook.call(accounts[0]);
         console.log("🚀 ~ file: SimpleStorage.js:23:", getQtyMyBook.toString()); // result getQtyMyBook = 1
